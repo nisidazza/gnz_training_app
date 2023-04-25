7f42615139f25657a7bffb3b525b223ef3fe377d
Nisida Azzalini <nisida.azzalini@xero.com>
2023-04-26 11:27:58 +1200

    wip: splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-
+import React, { useEffect, useState } from "react";
+import { SplashScreen } from "./SplashScreen";
 
 export const App = () => {
-  return (
-   <p> Cleanedup React App</p>
-  );
-}
+  const [initialLoading, setInitialLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setInitialLoading(false), 5000);
+  }, []);
+
+  return <>{initialLoading ? <SplashScreen /> : <p> Cleanedup React App</p>}</>;
+};
---
+++ src/SplashScreen.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+
+export const SplashScreen = () => {
+  return (
+    <>
+      <img src={"/images/GNZ_logo.png"} alt="GNZ logo" />
+      <Typography>GNZ Training App</Typography>
+    </>
+  );
+};
