9c90db616ced08adc0942a3c1be45cfd616666b9
Nisida Azzalini <nisida.azzalini@xero.com>
2023-04-26 16:22:10 +1200

    fix: move wrapper outside component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { SplashScreen } from "./SplashScreen";
-import jsonData from "./mock-data.json";
 import { Navigation } from "./MainMenu/Navigation";
 import styled from "styled-components";
 
-const data = JSON.parse(JSON.stringify(jsonData));
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  margin: auto;
+`;
 
 export const App = () => {
   const [initialLoading, setInitialLoading] = useState(true);
@@ -12,16 +19,6 @@ export const App = () => {
   useEffect(() => {
     setTimeout(() => setInitialLoading(false), 4000);
   }, []);
-
-  const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 100%;
-    margin: auto;
-  `;
 
   return (
     <Wrapper>{initialLoading ? <SplashScreen /> : <Navigation />}</Wrapper>
