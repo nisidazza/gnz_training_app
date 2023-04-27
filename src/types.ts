0e4e369783d8a1299be3cb84468f956e710cf1c8
Nisida Azzalini <nisida.azzalini@xero.com>
2023-04-27 17:11:03 +1200

    fix types

--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,17 @@
 export type ParentNode = {
-    children: ParentNodeChild[];
-    _parent: null;
-    name: string;
-  };
-  
-  export type ParentNodeChild = {
-    children: ParentNodeChild[] | LeafNode;
-    _parent: null;
-    name: string;
-  };
-  
-  export type LeafNode = {
-    _parent: null;
-    main_html_content?: string;
-    name: string;
-  };
+  children: ParentNodeChild[];
+  _parent: null;
+  name: string;
+}
+
+export type ParentNodeChild = {
+  children: (ParentNode | LeafNode)[];
+  _parent: null;
+  name: string;
+}
+
+export type LeafNode = {
+  _parent: null;
+  main_html_content?: string;
+  name: string;
+};
