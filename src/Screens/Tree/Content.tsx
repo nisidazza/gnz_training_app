e413dcfe4e2bb3980fcb3c9e9c420b6b6f30db4f
Nisida Azzalini <nisida.azzalini@xero.com>
2023-04-28 16:42:28 +1200

    rename placeholder component

---
+++ src/Screens/Tree/Content.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react";
+import { ContentNode } from "../../types";
+import { MenuList, MenuItem, Button } from "@mui/material";
+
+export const Content: FC<{
+  data: ContentNode;
+  setClickedName: React.Dispatch<React.SetStateAction<string | null>>;
+}> = ({ data, setClickedName }) => {
+  const nodeNames = data.children.map((obj) => obj.name);
+
+  return (
+    <MenuList>
+      {nodeNames.map((nodeName, i) => (
+        <MenuItem key={i}>
+          <Button variant="outlined" onClick={() => setClickedName(nodeName)}>
+            {nodeName}
+          </Button>
+        </MenuItem>
+      ))}
+    </MenuList>
+  );
+};
