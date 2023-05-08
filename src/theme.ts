3aa493654646d63596eae6ac75ebd64115b08694
nisidazza <nisida.azzalini@gmail.com>
2023-05-09 11:22:07 +1200

    override MUI style

--- src/theme.ts
+++ src/theme.ts
@@ -7,5 +7,37 @@ export const theme = createTheme({
         disableRipple: true,
       },
     },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          backgroundColor: '#57B3DF',
+          border: '1px solid #57B3DF',
+          color: 'white',
+
+          '&:hover': {
+            backgroundColor: '#4d92b3',
+            border: '1px solid #4d92b3',
+          }
+        }
+      }
+    },
+    MuiMenuItem: {
+      styleOverrides: {
+        root: {
+          '&:hover': {
+            backgroundColor: 'transparent',
+            border: 'none',
+          }
+        }
+      }
+    },
+    MuiLink: {
+      styleOverrides: {
+        root: {
+          color: "#777",
+          textDecoration: 'none',
+        }
+      }
+    }
   },
 });
