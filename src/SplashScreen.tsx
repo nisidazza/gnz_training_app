494ab208e91d34b4aa28cd1781d1a9fb48e29c32
nisidazza <nisida.azzalini@gmail.com>
2023-05-09 12:37:55 +1200

    splashscreen style

--- src/SplashScreen.tsx
+++ src/SplashScreen.tsx
@@ -1,10 +1,28 @@
 import { Box, Typography } from "@mui/material";
+import styled from "styled-components";
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 20px 0px;
+`;
 
 export const SplashScreen = () => {
   return (
-    <Box sx={{ width: "auto", height: "auto" }}>
-      <img src={"/images/GNZ_logo.png"} alt="GNZ logo" />
-      <Typography>GNZ Training App</Typography>
-    </Box>
+    <StyledContainer>
+      <Box sx={{ m: 2 }}>
+        <img
+          src={"/images/GNZ_logo.png"}
+          alt="GNZ logo"
+          height="auto"
+          width="100%"
+        />
+      </Box>
+      <Typography variant="h5" component="h2" mt={4}>
+        GNZ Training Program
+      </Typography>
+    </StyledContainer>
   );
 };
